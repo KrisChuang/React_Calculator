9eb4febb5805f6a321328f73eefddc9db03a8c49
KrisChuang <kris.kh.chuang@gmail.com>
2024-09-23 01:03:49 +0800

    First Class Function Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function MyButton(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button>
+      I'm a button
+    </button>
   );
 }
 
-export default App;
+function AboutPage(){
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+export default function MyApp(){
+  return (
+    <div>
+      <h1>Welcome to my App</h1>
+      <MyButton/>
+      <AboutPage/>
+    </div>
+  );
+}
